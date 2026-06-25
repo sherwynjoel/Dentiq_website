0f37deddca3ad237d4dc4f3dcd1692c2c7625dd0
Sherwyn Joel <sherwynjoel2004@email.com>
2026-06-25 10:57:20 +0530

    Refactor: key achievements format and configure/optimize deployment settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://wolfstride.in',
+  site: 'https://dentiq.in',
   output: 'static',
   integrations: [sitemap()],
   vite: {
